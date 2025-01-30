30e7e5cbd813aa7b61389dcb0a021ddd7cc47877
ashishpatel-dd1 <ashishbond00726@gmail.com>
2025-01-30 13:24:01 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# magic8ball
+
+A simple magic 8 ball, which can answer your daily life procrastination questions.
---
+++ android/app/src/main/java/org/soeuqinu/magic8ball/MainActivity.java
@@ -0,0 +1,6 @@
+package org.soeuqinu.magic8ball;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
